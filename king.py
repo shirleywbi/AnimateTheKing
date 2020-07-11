915afb4f85654142b280cf847a53ab7dda7d47e8
shirleywbi <swwbi@msn.com>
2020-07-10 22:24:38 -0700

    add to greyscale

--- king.py
+++ king.py
@@ -219,6 +219,23 @@ def start_fire():
             tags=tag
         )
 
+    def hex_to_rgb(hex):
+        h = hex.lstrip('#')
+        return tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+
+    def rgb_to_hex(rgb):
+        print(rgb)
+        return "#%02x%02x%02x" % rgb
+
+    def to_greyscale(hex, inc):
+        old_rgb = hex_to_rgb(hex)
+        max_rgb = max(old_rgb)
+        new_rgb = tuple(map(lambda x: x + min(inc, max_rgb - x) if (x < max_rgb) else x, old_rgb))
+        new_hex = rgb_to_hex(new_rgb)
+        canvas.itemconfig(KING, fill=new_hex)
+
+    to_greyscale(BASE_COLOR, 100)
+
     i, j, k = 0, -20, -40
     sleep_count = -40
     while True:
